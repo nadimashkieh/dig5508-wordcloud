a9c8e6ecc3bbe080bbb362b1730e99088146ce2a
nadimashkieh <59630860+nadimashkieh@users.noreply.github.com>
2020-11-29 00:34:35 -0500

    classes, functions

--- my_wordcloud.py
+++ my_wordcloud.py
@@ -6,54 +6,102 @@ from gutenberg.cleanup import strip_headers
 from gutenberg.acquire import get_metadata_cache
 from gutenberg.query import list_supported_metadatas
 from pprint import pprint
-import json
-from collections import defaultdict 
 import matplotlib.pyplot as plt 
-import pandas as pd 
-from pandas.io.json import json_normalize
-import numpy as np
-#import pandas as pd
-from os import path
-from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
-import matplotlib.pyplot as plt
-#% matplotlib inline
 
-#c:\intelpython3\lib\site-packages\matplotlib\__init__.py:
-import warnings
-warnings.filterwarnings("ignore")
+class book:
+    def __init__(self, index, author, title, subject ):
+        self.index = index
+        self.author = author
+        self.title = title
+        self.subject = subject
+    
+    def get_book_author( self ):
+        return self.author
+
+    def get_book_title( self ):
+        return self.title
+    
+    def get_book_index(self):
+        return self.index
+
+    def get_book_subject(self):
+        return self.subject
 
 
-cache = get_metadata_cache()
-#cache.populate()
+class book_catalog:
+    def __init__( self ):
+        self.books = []
 
+    def get_books( self ):
+        return self.books
 
-cache.open()
-graph = cache.graph
+    def add_book( self, book ):
+        self.books.append( book )
 
+    def get_book( self, index ):
+        for book in self.books:
+            if int(book.index) == int(index):
+                return book
+        return( None )
 
+    def get_authors( self ):
+        authors = []
+        for book in self.books:
+            if book.author not in authors:
+                authors.append( book.author )
+        authors.sort()
+        for author in authors:
+            print( author )
+
+    def get_titles( self ):
+        titles = []
+        for book in self.books:
+            if book.title not in titles:
+                titles.append( book.title )
+        return titles.sort()      
+
+    def get_subjects( self ):
+        subjects = []
+        for book in self.books:
+            if book.subject not in subjects:
+                subjects.append( book.subject )
+        return subjects
+
+    def display_titles_by_author( self ):
+        authors = {}
+        for book in self.books:
+            authors[ book.author ] = str( book.index ) + ') ' + '[ ' + book.title + ' ]'
+
+        for author in authors:
+            print( f"{'[ ' + author + ' ]':>50}" + ' ==> ' + authors[ author ] )
+        return
+##
+##
+##
 def get_all_titles():
-    my_catalog = {}
+    my_catalog = book_catalog()
 
     for i in range(1, 65000):
-        my_title_list = [] 
-        title = get_metadata('title', i )
-        if(title):
+
+        title = ''.join( list( get_metadata( 'title', i ) ) )
+
+        if( title ):
             lang = list( get_metadata('language', i ))[0]
-            if lang == 'en':
-                my_title_list.append( list( get_metadata('author', i )))
-                my_title_list.append( i )
-                my_catalog[ list(title)[0] ] = my_title_list
-    return(my_catalog)
+            if lang == 'en':   
+                my_book = book( 
+                        i, 
+                        ''.join( list( get_metadata( 'author', i ) ) ).replace("\n", " ").replace("\r", " "), 
+                        ''.join( list( get_metadata( 'title', i ) ) ).replace("\n", " ").replace("\r", " "),
+                        ''.join( list( get_metadata( 'subject', i ) ) ).replace("\n", " ").replace("\r", " ") 
+                        )  
+                my_catalog.add_book( my_book )
+    return( my_catalog )
 
 
+def display_word_cloud( text ):
 
-
-def display(text):
-
-    #df = pd.DataFrame(book,columns=['title','author', 'number', 'text'])
-   
     comment_words = '' 
     stopwords = set(STOPWORDS) 
     
@@ -69,7 +117,6 @@ def display(text):
     comment_words += " ".join(tokens)+" "
     #pprint( my_word_frequency)
 
-
     wordcloud = WordCloud(width = 800, height = 800, 
                     background_color ='white', 
                     stopwords = stopwords, 
@@ -84,45 +131,76 @@ def display(text):
     plt.show()
 
 
+def search_display_options( my_catalog ):
+    search_result_catalog = book_catalog()
 
-def display_options(catalog):
+    search_type = input('Please select a search type: Author, Subject, Title [Aa/Ss/Tt]:  ')
 
-    search_term = input( 'Please enter a search term for an author or a title: ')
+    if  search_type == 'A' or search_type == 'a':
+        search_term = input( 'Please enter a search term for an Author: ')
+    elif search_type == 'T' or search_type == 't' :
+        search_term = input( 'Please enter a search term for a Title: ')
+    elif search_type == 'S' or search_type == 's':
+        search_term = input( 'Please enter a search term for a Subject: ')
+    else:
+        print( 'Invalid search type...')
+        return
 
+    # set match flag to false
     match = False
-
-    for key in catalog:
-        #if key.find( search_term ) != -1 :
-        if set( key.split(' ') ).intersection( set(search_term.split(' ') ) ):
-            print( str( catalog[ key ][1]) + ') ' + key )
+    # fill up a set of all the titles that match the search
+    for my_book in my_catalog.get_books():
+        if ( search_type == 'a' or search_type == 'A' )  and set( my_book.get_book_author().lower().split(' ') ).intersection( set( search_term.lower().split( ' ' ) ) ) :
+            search_result_catalog.add_book( my_book )
             match = True
-    
+
+        if ( search_type == 't' or search_type == 'T' )  and set( my_book.get_book_title().lower().split(' ')  ).intersection( set( search_term.lower().split( ' ' ) ) ) :
+            search_result_catalog.add_book( my_book )
+            match = True
+
+        if ( search_type == 's' or search_type == 'S' )  and set( my_book.get_book_subject().lower().split(' ')  ).intersection( set( search_term.lower().split( ' ' ) ) ) :
+            search_result_catalog.add_book( my_book )
+            match = True
+
+    # for my_book in search_result_catalog.get_books():
+    #     print( str( my_book.get_book_index() ) + ') ' + '[ Author: ' + my_book.get_book_author() + '] [Title: ' + my_book.get_book_title() + ']')
+
+    search_result_catalog.display_titles_by_author()
+    search_result_catalog.get_authors()
+
     if match:
         title_num = input( 'Please type a title number from the above list: ')
-        title_text = list(get_metadata('title', title_num ))[0]
-        print( 'You selected: ' + title_text )
+   
+        print( 'Displaying Word Cloud in [Subject: ' + my_book.get_book_subject() + '] for [Title: ' + my_book.get_book_title() + '] by [Author:' + my_book.get_book_author() + ']'  )
         try:
-            return( strip_headers(load_etext( int(title_num) )).strip() )
+            my_book = search_result_catalog.get_book( title_num )
+            return( strip_headers( load_etext( int( title_num ) ) ).strip() )
         except:
-            print ('Failed to find a textual download candidate for ' + title_text )
+            print ('Failed to find a textual download candidate for ' + my_book.get_book_title() )
             return(None)
     else:
-        print('No matches found... ')
+        print('No matches found for [' + search_term + ']...')
         return(None)
     
     #display(json.dumps( { "title": title_text, "author": catalog[ title_text ][ 0 ], "number": catalog[ title_text ][ 1 ], "text": text } ))
     #display( json.dumps( [ title_text, catalog[ title_text ][ 0 ], catalog[ title_text ][ 1 ], text ] ) )
-    
-  
 
 
-catalog = get_all_titles()
+##
+## main
+##
+cache = get_metadata_cache()
+#cache.populate()
+cache.open()
+graph = cache.graph
+my_catalog = get_all_titles()
+
 more_titles = 'y'
 while more_titles == 'y' or more_titles == 'Y':
-    text = display_options( catalog )
-    if text:
-        display( text )
-    more_titles = input('Would you like to search for another title: [y/n]')
+    book_text = search_display_options( my_catalog )
+    if book_text:
+        display_word_cloud( book_text )
+    more_titles = input('Would you like to search again: [y/n]')
 
 
 cache.close()
